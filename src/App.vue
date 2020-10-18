332fb06395f26f234beb03ee21f720442d50ce0d
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-10-18 17:24:26 +0200

    Change cards wrappers to be more responsive

--- src/App.vue
+++ src/App.vue
@@ -23,22 +23,22 @@
 
         <v-row>
 
-          <v-col cols="4">
+          <v-col cols="12" md="6" lg="4">
             <base-card title="Input data" icon="text-subject">
               <v-textarea v-model="text" @input="deleteFile" :label="textLabel" filled dense/>
               <v-file-input @change="readFile" v-model="textFile" label="Plaintext file" filled dense/>
             </base-card>
           </v-col>
 
-          <v-col cols="4">
+          <v-col cols="12" md="6" lg="4">
             <base-card title="Secrets" icon="key-outline">
-              <AlphabetTable @input="updateAlphabet"/>
+              <AlphabetTable @input="updateAlphabet" :size="alphabetMatrixSize"/>
               <v-divider class="my-3"/>
-              <v-text-field v-model="encryptionKey" label="Key" filled dense/>
+              <v-text-field v-model="encryptionKey" :rules="encryptionKeyRules" label="Key" filled dense/>
             </base-card>
           </v-col>
 
-          <v-col cols="4">
+          <v-col cols="12" md="6" lg="4">
             <base-card title="Result" icon="lock-outline">
               <v-textarea :value="result" label="Encrypted text" readonly filled dense/>
             </base-card>
