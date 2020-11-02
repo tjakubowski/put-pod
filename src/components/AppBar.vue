961ec848bcd06edd24570e73733767cd615ec860
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-11-02 17:19:59 +0100

    Move code to separate files

---
+++ src/components/AppBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar app flat color="white">
+    <v-toolbar-title>Nihilist substitution</v-toolbar-title>
+    <v-spacer/>
+    <v-dialog
+      v-model="helpDialog"
+      width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          icon
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-help-circle-outline</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Jak to działa?</span>
+        </v-card-title>
+        <help-panels/>
+      </v-card>
+    </v-dialog>
+  </v-app-bar>
+</template>
+
+<script>
+import HelpPanels from "@/components/HelpPanels";
+export default {
+  name: 'AppBar',
+  components: {HelpPanels},
+  data() {
+    return {
+      helpDialog: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer padless>
+    <v-card class="flex" flat tile>
+      <v-card-text class="text-center">
+        Tymon Jakubowski
+      </v-card-text>
+    </v-card>
+  </v-footer>
+
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
