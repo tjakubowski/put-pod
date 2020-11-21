c8ae261ce2198284f6db76e857271c4142c7cee3
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-11-21 18:28:42 +0100

    Add validation in LfsrEdition component

--- src/components/LfsrEditor.vue
+++ src/components/LfsrEditor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <v-form ref="form">
     <v-text-field
       clearable
       counter
@@ -15,10 +15,11 @@
       small-chips
       type="number"
       :append-icon="null"
+      :rules="polynomialRules"
       :label="polynomialLabel"
       v-model="polynomial"
     />
-  </div>
+  </v-form>
 </template>
 
 <script>
@@ -36,10 +37,22 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      polynomialRules: [
+        (v) => {
+          const maxPolynomial = this.lfsr.getMaxPolynomial();
+          return v.every((char) => +char <= maxPolynomial)
+          || `Max polynomial degree is ${maxPolynomial}`;
+        },
+      ],
+    };
+  },
   computed: {
     state: {
       set(value) {
         this.lfsr.initialState = value;
+        this.$refs.form.validate();
       },
       get() {
         return this.lfsr.state.join('');
