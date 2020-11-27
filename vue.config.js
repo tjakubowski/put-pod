54fcbf56cab534fbe32d291b1dae256642c5063f
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-11-27 01:42:11 +0100

    Delete worker config in webpack

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,7 @@
-const WorkerPlugin = require('./src/workers');
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/put-pod/' : '/',
   transpileDependencies: [
     'vuetify',
   ],
   lintOnSave: true,
-  configureWebpack: {
-    output: {
-      globalObject: 'this',
-    },
-    plugins: [
-      new WorkerPlugin(),
-    ],
-  },
-
 };
