ebfe08e48610f6a85a8f808152e29ba9b83ea5c4
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-11-30 16:14:26 +0100

    Add decrypting in StreamCipher

--- src/pages/StreamCipher.vue
+++ src/pages/StreamCipher.vue
@@ -1,39 +1,52 @@
 <template>
-  <v-row>
+  <div>
     <portal to="app-info">
       <info-panels :panels="panels"/>
     </portal>
 
-    <v-col cols="12" md="6" lg="3">
-      <base-card title="Input data" icon="text-subject">
-        <v-form ref="inputForm" v-model="isInputDataValid">
-          <file-btn class="mb-3" text block color="primary" @input="importData($event, 'generatedBits')">
-            <v-icon left v-text="'mdi-import'"/> Import bits stream
-          </file-btn>
-          <v-textarea counter outlined dense height="200" label="Generated bits" :rules="generatedBitsRules" v-model="generatedBits"/>
+    <v-row>
+      <v-col class="text-right">
+        <v-btn outlined text @click="reverse">
+          <v-icon left v-text="'mdi-swap-vertical'" :class="{'v-icon--rotate': !encrypt}"/>
+          {{ encrypt ? 'Encrypt' : 'Decrypt' }}
+        </v-btn>
+      </v-col>
+    </v-row>
 
-          <v-divider/>
+    <v-row>
 
-          <file-btn class="my-3" text block color="primary" @input="importData($event, 'inputText')">
-            <v-icon left v-text="'mdi-import'"/> Import text
-          </file-btn>
-          <v-textarea counter outlined dense height="200" label="Input text" :rules="inputTextRules" v-model="inputText"/>
-        </v-form>
-      </base-card>
-    </v-col>
+      <v-col cols="12" md="6" lg="3">
+        <base-card title="Input data" icon="text-subject">
+          <v-form ref="inputForm" v-model="isInputDataValid">
+            <file-btn class="mb-3" text block color="primary" @input="importData($event, 'generatedBits')">
+              <v-icon left v-text="'mdi-import'"/> Import bits stream
+            </file-btn>
+            <v-textarea counter outlined dense height="200" label="Generated bits" :rules="generatedBitsRules" v-model="generatedBits"/>
 
-    <v-col cols="12" lg="6">
-      <base-card title="Result" icon="numeric">
-        <template v-slot:actions>
-          <v-btn text color="primary" @click="downloadResult" :disabled="!isDone">
-            <v-icon left v-text="'mdi-download'"/> Download
-          </v-btn>
-        </template>
-        <v-textarea readonly counter outlined dense height="200" label="Generated bits" class="mb-3" :value="result"/>
-      </base-card>
-    </v-col>
+            <v-divider/>
 
-  </v-row>
+            <file-btn class="my-3" text block color="primary" @input="importData($event, 'inputData')">
+              <v-icon left v-text="'mdi-import'"/> Import data
+            </file-btn>
+            <v-textarea counter outlined dense height="200" label="Input data" :rules="inputDataRules" v-model="inputData"/>
+          </v-form>
+        </base-card>
+      </v-col>
+
+      <v-col cols="12" lg="6">
+        <base-card title="Result" icon="numeric">
+          <template v-slot:actions>
+            <v-btn text color="primary" @click="downloadResult" :disabled="!isDone">
+              <v-icon left v-text="'mdi-download'"/> Download
+            </v-btn>
+          </template>
+          <v-textarea readonly counter outlined dense height="200" label="Result" class="mb-3" :value="result"/>
+        </base-card>
+      </v-col>
+
+    </v-row>
+
+  </div>
 </template>
 
 <script>
@@ -50,21 +63,23 @@ export default {
   data() {
     return {
       panels,
+      encrypt: true,
       isInputDataValid: false,
       generatedBits: '',
-      inputText: '',
+      inputData: '',
       generatedBitsRules: [
         (v) => v.length > 0 || 'Generated bits are required',
-        (v) => [...v].every((char) => ['0', '1'].includes(char)) || 'Generated bits must be a binary string',
-        (v) => v.length >= this.inputTextBitsLength || `Binary stream length must be equal or greater than ${this.inputTextBitsLength}`,
+        (v) => this.isBinaryString(v) || 'Generated bits must be a binary string',
+        (v) => v.length >= this.inputDataLength || `Binary stream length must be equal or greater than ${this.inputDataLength}`,
       ],
-      inputTextRules: [
-        (v) => v.length > 0 || 'Input text is required',
+      inputDataRules: [
+        (v) => v.length > 0 || 'Input data is required',
+        (v) => this.encrypt || (!this.encrypt && this.isBinaryString(v)) || 'Input data must be a binary string',
       ],
     };
   },
   watch: {
-    inputText() {
+    inputData() {
       this.$refs.inputForm.validate();
     },
   },
@@ -72,20 +87,29 @@ export default {
     isDone() {
       return this.result.length > 0;
     },
-    inputTextBitsLength() {
-      return this.inputText.length * 8;
+    inputDataLength() {
+      return this.inputDataBits.length;
     },
-    inputTextBits() {
-      return [...this.inputText]
+    inputDataBits() {
+      if (!this.encrypt) return this.inputData;
+
+      return [...this.inputData]
         .map((char) => char.charCodeAt(0).toString(2).padStart(8, '0'))
         .join('');
     },
     result() {
       if (!this.isInputDataValid) return '';
 
-      return [...this.inputTextBits]
-        .map((bit, index) => +bit ^ +this.generatedBits[index])
-        .join('');
+      const xorResult = [...this.inputDataBits]
+        .map((bit, index) => +bit ^ +this.generatedBits[index]).join('');
+
+      if (this.encrypt) return xorResult;
+
+      const byteSize = 8;
+      const bytes = Array.from({ length: Math.ceil(xorResult.length / byteSize) },
+        (v, i) => xorResult.slice(i * byteSize, i * byteSize + byteSize));
+
+      return bytes.map((byte) => String.fromCharCode(parseInt(byte, 2))).join('');
     },
   },
   methods: {
@@ -96,6 +120,13 @@ export default {
       readFile(file, (result) => {
         this.$set(this, target, result);
       });
+    },
+    isBinaryString(string) {
+      return [...string].every((char) => ['0', '1'].includes(char));
+    },
+    reverse() {
+      this.inputData = this.result;
+      this.encrypt = !this.encrypt;
     },
   },
 };
